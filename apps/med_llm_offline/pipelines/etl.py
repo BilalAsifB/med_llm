a3e3e752c4eb62cebc02dbb9fecbc76d6178e219
BilalAsifB <bilalburney14@gmail.com>
2025-07-18 05:01:09 +0500

    Removed local save from etl pipeline.

--- apps/med_llm_offline/pipelines/etl.py
+++ apps/med_llm_offline/pipelines/etl.py
@@ -5,22 +5,16 @@ from zenml import pipeline
 
 from steps.etl import crawl
 from steps.infrastructure import (
-    ingest_to_mongodb,
-    save_documents_to_disk,
+    ingest_to_mongodb
 )
 
 @pipeline
 def etl(
-    data_dir: Path,
     load_collection_name: str,
     max_workers: int = 10,
     base_url: str = "https://www.dvago.pk",
 ) -> None:
-    crawled_data_dir = data_dir / "dvago"
     crawled_data = crawl(max_workers=max_workers, base_url=base_url)
-
-    logger.info(f"Saving crawled data to {crawled_data_dir}")
-    save_documents_to_disk(documents=crawled_data, output_dir=crawled_data_dir)
 
     logger.info(
         f"Saving crawled data to MongoDB collection '{load_collection_name}'"
